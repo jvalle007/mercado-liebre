ade3d0ae39a18606e725e7d50641f24a5b3a53e6
jvalle007 <julian.gabriel.valle@gmail.com>
2021-11-22 01:02:25 -0300

    agregando paginas

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const express = require('express');
+const express = require("express")
+const app = express()
+const port = 3000
+const path = require("path")
+app.use(express.static("public"));
 
-const app = express ();
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/home.html"))
+})
 
-app.listen (3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-});
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/register.html"))
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/login.html"))
+})
+
+app.post('/', (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/home.html"))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
